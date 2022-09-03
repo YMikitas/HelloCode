527b7784f6881fa41e7b3aa8bf3a646ec950b9f7
YMikitas <ymikitas@gmail.com>
2022-09-03 21:25:17 +0300

    Add Task 9

--- Lesson1/Seminar1/Program.cs
+++ Lesson1/Seminar1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 9
+// Напишите программу, которая выводит случайное число из отрезка 10-99 и показывает наибольшую цифру числа
+// 78 -> 8
+// 75 -> 7
+
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;  // 78 / 10 = 7
+int secondDigit = number % 10; // Остаток от деления 78 на 10 = 8
+int maximum = firstDigit;
+if(secondDigit > maximum) maximum = secondDigit;
+
+System.Console.WriteLine("Наибольшая цифра в числе " + number + " - это " + maximum); // Конкатенация - процесс сложения строк
+// Или
+System.Console.WriteLine($"Наибольшая цифра в числе {number} - это {maximum}"); // Интерполяция
+
